23bda91f47113fc3398cde0f9a9d5b079a9b9992
pricem3 <Matthew.G.Price@ext.ons.gov.uk>
2020-07-24 11:45:29 +0100

    j773: Move union dataframe function from main to helper module
    As this is a utility function it needn't be in the main file.

--- cprices/utils/helpers.py
+++ cprices/utils/helpers.py
@@ -1,11 +1,20 @@
 """Contains methods:
-- find USED IN MAIN.PY
-
+- find
+    used in union_dfs_from_all_scenarios
+- union_dfs_from_all_scenarios
+    used in main.py
 """
+# import pyspark libraries
+from pyspark.sql import DataFrame
+from pyspark.sql import functions as F
+
+# import python libraries
+from functools import reduce
+
 
 def find(key, dictionary):
     """
-    Returns all values in a dictionary (with nested dictionaries) that have 
+    Returns all values in a dictionary (with nested dictionaries) that have
     to the user-specified key.
 
     Parameters
@@ -31,3 +40,61 @@ def find(key, dictionary):
                 for result in find(key, d):
                     yield result
 
+
+def union_dfs_from_all_scenarios(dfs):
+    """
+    Unions the corresponding dataframes from all scenarios so in the output
+    dictionary, each stage has one dataframe. Before doing that, a scenario
+    column is added to each dataframe to distinguish between scenarios.
+
+    Parameters
+    ----------
+    dfs : nested dictionary of spark dataframes
+        Every key in the dfs dictionary holds the dataframes for the stages of
+        the scenario run.
+
+    scenario : string
+        The name of the scenario, i.e. scenario_1, scenario_2, etc
+
+    Returns
+    -------
+    dfs : dictionary of spark dataframes
+        Each key holds the unioned dataframe across all scenarios for a
+        particular stage.
+    """
+    # ADD SCENARIO COLUMN TO ALL DATAFRAMES
+
+    for scenario in dfs:
+        # scenarios have names: scenario_x, extract number
+        scenario_no = scenario.split('_')[1]
+        for df in dfs[scenario]:
+            dfs[scenario][df] = (
+                dfs[scenario][df]
+                .withColumn(
+                    'scenario',
+                    F.lit(scenario_no)
+                )
+            )
+
+    # UNION DATAFRAMES (IF THERE ARE MORE THAN 1 SCENARIOS)
+
+    # name of first scenario
+    scenario = list(dfs.keys())[0]
+
+    if len(dfs) > 1:
+        # names of dataframes - they are the same for each scenario
+        # they can be collected from any scenario in dfs
+        names = dfs[scenario].keys()
+
+        dfs_unioned = {}
+        for name in names:
+            dfs_to_union = list(find(name, dfs))
+            dfs_unioned[name] = reduce(DataFrame.unionByName, dfs_to_union)
+            dfs_unioned[name].cache().count()
+
+        dfs = dfs_unioned
+
+    else:
+        dfs = dfs[scenario]
+
+    return dfs
