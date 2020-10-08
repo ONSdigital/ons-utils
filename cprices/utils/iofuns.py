637c37d005890b8cc4f346a96413c2f9b355e099
pricem3 <matthew.g.price@ons.gov.uk>
2020-10-08 14:24:10 +0000

    PADSP-914 apply pep to iofuncs module

--- cprices/utils/iofuns.py
+++ cprices/utils/iofuns.py
@@ -1,122 +1,130 @@
-# import spark libraries
-from pyspark.sql import DataFrame
-
+"""I/O functions for pipeline interaction with HDFS."""
 # import python libraries
-import os
 import copy
 from datetime import datetime
-from functools import reduce
-from importlib import reload
 import logging
+import os
+from typing import Dict
+
+# import pyspark libraries
+from pyspark.sql import DataFrame as sparkDF
+from pyspark.sql import SparkSession
 
 
 LOGGER = logging.getLogger()
 
 
-def load_input_data(spark, config, dev_config): 
-    """
-    Creates a dictionary of spark dataframes from the staged data to feed into 
-    the core pipepline.
-    
+def load_input_data(
+    spark: SparkSession,
+    input_data: dict,
+    staged_dir: str,
+) -> Dict[dict, sparkDF]:
+    """Load data for processing as specified in the scenario config.
+
     Parameters
     ----------
-    spark : 
-        spark session
-        
-    config : python file/module 
-        It has the staged_data dictionary with all the data sources, suppliers
-        and items. Each combination is a path of dictionary keys that lead to a
-        value. This is initialised as an empty dictionary {}.
-        
-    dev_config : python file/module 
-        It has the path to the HDFS directory from where the staged data will 
-        be imported.
-        
+    spark: spark session
+
+    input_data: nested dictionary
+        Dictionary with all the data sources, suppliers and items. Each
+        combination is a path of dictionary keys that lead to a value. This is
+        initialised as an empty dictionary {}.
+
+    staged_dir: string
+        The path to the HDFS directory from where the staged data is located.
+
     Returns
     -------
-    dfs : dictionary of spark dataframes 
-        Each path of keys leads to a value/spark dataframe as it was read 
+    staged_dir : dictionary of spark dataframes
+        Each path of keys leads to a value/spark dataframe as it was read
         from HDFS for the corresponding table.
-        
     """
-    
-    staged_dir = dev_config.staged_dir
-    staged_data = copy.deepcopy(config.input_data)
-    
-    # webscraped data
-    for data_source in staged_data:
+    # Create a full copy of the input_data dictionary
+    staged_data = copy.deepcopy(input_data)
+
+    for data_source in input_data:
+
+        # webscraped data has 3 levels: data_source, supplier, item
         if data_source == 'web_scraped':
-            for supplier in staged_data[data_source]:
-                for item in staged_data[data_source][supplier]:
+            for supplier in input_data[data_source]:
+                for item in input_data[data_source][supplier]:
                     path = os.path.join(
-                        staged_dir, 
-                        data_source, 
-                        supplier, 
+                        staged_dir,
+                        data_source,
+                        supplier,
                         item+'.parquet'
                     )
+
                     staged_data[data_source][supplier][item] = (
                         spark
                         .read
                         .parquet(path)
                     )
-        elif data_source in ['scanner', 'conventional']:
-            for supplier in staged_data[data_source]:
-                path = os.path.join(staged_dir, data_source,supplier)
+
+        # conventional and scanner data has 2 levels: data_source, supplier
+        elif data_source in ('conventional', 'scanner'):
+            for supplier in input_data[data_source]:
+                path = os.path.join(staged_dir, data_source, supplier)
+
                 staged_data[data_source][supplier] = spark.read.parquet(path)
-    
+
     return staged_data
 
-    
-def save_output_hdfs(dfs, dev_config):
-    
-    """
-    Stores output dataframes in HDFS.
-    
+
+def save_output_hdfs(
+    dfs: Dict[str, sparkDF],
+    processed_dir: str,
+) -> str:
+    """Store output dataframes (combined across all scenarios) in HDFS.
+
     Parameters
     ----------
-    dfs : dictionary of spark dataframes
+    dfs: Dictionary of spark dataframes
         The output dataframes from all scenarios to store in HDFS.
-        
-    dev_config : python file/module 
+
+    processed_dir: string
         It has the path to the HDFS directory where the dfs will be stored.
-                         
+
+    Returns
+    -------
+    run_id: string
+        The unique identifying string for the run, of the form
+        current date, time and username (YYYYMMDD_HHMMSS_username).
+
     Notes
     -----
-    The run_id consists of the current date, time and username
-    (YYYYMMDD_HHMMSS_username). That's the name of the folder that will be
-    created inside the processed data folder in HDFS for this particular 
-    run and will contain all the output dataframes. 
-    The run_id is printed on the screen for the user to explore the output 
-    data.
-    
-    The configuration dataset is a two-column table where the first column 
+    That's the name of the folder that will be created inside the processed
+    data folder in HDFS for this particular run and will contain all the
+    output dataframes. The run_id is printed on the screen for the user to
+    explore the output data.
+
+    The configuration dataset is a two-column table where the first column
     shows the stage of the core pipeline and the second column shows (as a
     dictionary) all the config parameters for the corresponding stage. This
-    can be used as a reference for the user in case they want to check the 
+    can be used as a reference for the user in case they want to check the
     configuration of this run.
-        
     """
-    
     # create run id using username and current time
     username = os.environ['HADOOP_USER_NAME']
     current_date_time = datetime.now().strftime("%Y%m%d_%H%M%S")
     run_id = '_'.join([current_date_time, username])
-    
+
     # create directory path to export processed data
-    processed_dir = os.path.join(dev_config.processed_dir, run_id)
-        
+    processed_dir = os.path.join(processed_dir, run_id)
+
     for name in dfs:
         LOGGER.info(f'{name}...')
         if name in ['analysis']:
             # store analysis output as csv
-            df = dfs[name] 
-            df = df.repartition(1)
-            df.cache().count()
-            path = os.path.join(processed_dir, 'analysis')    
-            df.write.save(path, header=True, format='csv', mode='overwrite')
+            path = os.path.join(processed_dir, 'analysis')
+            dfs[name].repartition(1).write.csv(
+                path,
+                header=True,
+                mode='overwrite'
+            )
+
         else:
             path = os.path.join(processed_dir, name)
-            dfs[name].write.parquet(path)  
-            
-    return run_id
+            dfs[name].write.parquet(path)
 
+    return run_id
