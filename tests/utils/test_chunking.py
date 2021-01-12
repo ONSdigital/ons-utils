76aed259bef12d6df079958ace1cd08ab5e86b54
edmunm <Mitchell.Edmunds@ext.ons.gov.uk>
2021-01-12 12:50:14 +0000

    PADSP-1011: Added chunking for local collection
    Added the chunking functions to chunking.py in utils and implemented the
    chunking in main.py.

---
+++ tests/utils/test_chunking.py
@@ -0,0 +1,31 @@
+"""A set of unit tests for the chunking functions."""
+# # from numpy import nan
+# import pandas as pd
+# # from pandas import Timestamp
+# from pandas._testing import assert_frame_equal    #, assert_series_equal
+import pytest
+
+# from cprices.utils import chunking
+# from tests.conftest import (
+#     create_dataframe,
+#     # Case,
+#     # parametrize_cases,
+# )
+
+
+@pytest.mark.skip(reason="test shell")
+def test_get_chunks():
+    """Test for this."""
+    pass
+
+
+@pytest.mark.skip(reason="test shell")
+def test_align_chunks():
+    """Test for this."""
+    pass
+
+
+@pytest.mark.skip(reason="test shell")
+def test_chunk_up():
+    """Test for this."""
+    pass
