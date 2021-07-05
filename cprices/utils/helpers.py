88c591584bd3ad86fd573ccd5dcb13ca8ca49080
edmunm <Mitchell.Edmunds@ext.ons.gov.uk>
2021-07-05 16:06:58 +0000

    PADSP-1416: Remove `find` helper func - no longer used

--- cprices/utils/helpers.py
+++ cprices/utils/helpers.py
@@ -1,12 +1,7 @@
 """Miscellaneous helper functions."""
 # Import Python libraries.
 import logging
-from typing import (
-    Mapping,
-    Iterator,
-    Any,
-    List,
-)
+from typing import Mapping, Any, List
 
 # Import third party libraries.
 from flatten_dict import flatten, unflatten
@@ -23,34 +18,6 @@ def timer_args(name):
         'text': lambda secs: name + f": {format_timespan(secs)}",
         'logger': LOGGER.info,
     }
-
-
-# This function is no longer used - can be kept if useful?
-def find(key: str, dictionary: Mapping[str, Any]) -> Iterator[Any]:
-    """
-    Return all values in a nested dictionary that match key.
-
-    Parameters
-    ----------
-    key : string
-        The key to look for inside the dictionary.
-    dictionary : python dictionary
-        The dictionary to look for the user-specified key.
-
-    Returns
-    -------
-    Python generator object - can be turned into a list.
-    """
-    for k, v in dictionary.items():
-        if k == key:
-            yield v
-        elif isinstance(v, dict):
-            for result in find(key, v):
-                yield result
-        elif isinstance(v, list):
-            for d in v:
-                for result in find(key, d):
-                    yield result
 
 
 def _list_convert(x: Any) -> List[Any]:
--- tests/utils/test_helpers.py
+++ tests/utils/test_helpers.py
@@ -1,66 +1,11 @@
 """Tests for the helpers module."""
 import pytest
-from pyspark.sql import types as T
-from epds_utils.testing.pyspark_test_case import PySparkTest
-
-from cprices.utils import helpers
 
 
-class TestUtils(PySparkTest):
-    """Test class for utils functions: create_dataframe and find."""
-
-    def input_data_find(self):
-        """Create unit test input data for find"""
-        input_data = [(12345, 1), (23456, 2)]
-        schema = T.StructType([
-            T.StructField("col1", T.IntegerType(), False),
-            T.StructField("col2", T.IntegerType(), False)
-        ])
-        df1 = self.spark.createDataFrame(input_data, schema)
-
-        input_data = [(10000, 1), (20000, 2)]
-        schema = T.StructType([
-            T.StructField("col1", T.IntegerType(), False),
-            T.StructField("col2", T.IntegerType(), False)
-        ])
-        df2 = self.spark.createDataFrame(input_data, schema)
-
-        dictionary = {
-            "key1": "value1",
-            "key2": {
-                "subkey1": df1,
-                "subkey2": {
-                    "subkey3": df2,
-                    "subkey4": "value2"
-                }
-            }
-        }
-
-        return df1, df2, dictionary
-
-    def test_find(self):
-        """Unit test for find"""
-        df1, df2, dictionary = self.input_data_find()
-
-        # Subtest 1
-        key = "key1"
-        dictionary = {"key1": "value1"}
-        value = "value1"
-        generator = helpers.find(key, dictionary)
-        for v in generator:
-            self.assertEqual(value, v)
-
-        # Subtest 2
-        key = "subkey1"
-        generator = helpers.find(key, dictionary)
-        for v in generator:
-            self.assertDFEqual(df1, v, rounding_scale=3)
-
-        # Subtest 3
-        key = "subkey3"
-        generator = helpers.find(key, dictionary)
-        for v in generator:
-            self.assertDFEqual(df2, v, rounding_scale=3)
+@pytest.mark.skip(reason="test shell")
+def test_timer_args():
+    """Test for this."""
+    pass
 
 
 @pytest.mark.skip(reason="test shell")
