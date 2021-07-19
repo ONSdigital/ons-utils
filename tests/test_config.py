3128c0c4f4c5c57d618af9fbef65e3eaf4a8fb0d
edmunm <Mitchell.Edmunds@ext.ons.gov.uk>
2021-07-19 16:36:52 +0000

    PADSP-1416: Finish test suite for Config base class

--- tests/test_config.py
+++ tests/test_config.py
@@ -6,7 +6,7 @@ from cprices.config import *
 from cprices import config
 
 
-def write_config_yaml(dir, yaml_input: str) -> None:
+def write_config_yaml(dir, yaml_input: str = "my_attr: test") -> None:
     """Write a file called my_config.yaml with given yaml at given dir."""
     with open(dir.join('my_config.yaml'), 'w') as f:
         yaml.dump(yaml.safe_load(yaml_input), f)
@@ -15,7 +15,7 @@ def write_config_yaml(dir, yaml_input: str) -> None:
 @pytest.fixture
 def test_config(tmpdir, monkeypatch):
     """Sets up a test config file in tmpdir/config with given yaml."""
-    def _(yaml_input: str = "template_text") -> Config:
+    def _(yaml_input: str = "my_attr: test") -> Config:
         config_dir = tmpdir.mkdir('config')
         monkeypatch.setattr(config.Path, 'home', lambda: Path(tmpdir))
         write_config_yaml(config_dir, yaml_input=yaml_input)
@@ -41,7 +41,7 @@ class TestConfig:
         returns the locations given by the env var.
         """
         monkeypatch.setenv('CPRICES_CONFIG', str(tmpdir))
-        write_config_yaml(tmpdir, yaml_input="template_text")
+        write_config_yaml(tmpdir)
         conf = Config('my_config')
         assert conf.get_config_dir() == tmpdir
 
@@ -59,7 +59,7 @@ class TestConfig:
         monkeypatch.setattr(config.Path, 'cwd', lambda: Path(tmpdir))
         monkeypatch.setattr(config.Path, 'home', lambda: Path(tmpdir))
 
-        write_config_yaml(target_dir, yaml_input="template_text")
+        write_config_yaml(target_dir)
 
         assert Config('my_config').get_config_dir() == target_dir
 
@@ -72,7 +72,7 @@ class TestConfig:
         monkeypatch.setattr(config.Path, 'cwd', lambda: Path(tmpdir))
         monkeypatch.setattr(config.Path, 'home', lambda: Path(tmpdir))
 
-        write_config_yaml(target_dir, yaml_input="template_text")
+        write_config_yaml(target_dir)
 
         assert Config('my_config').get_config_dir() == target_dir
 
@@ -94,47 +94,162 @@ class TestConfig:
             - referee
             - dog
         """)
-        print(conf.load_config())
+
         assert conf.load_config() == {
             'bells': {'big_ben': {'dongs': 12}},
             'whistles': ['referee', 'dog'],
         }
 
-    @pytest.mark.skip(reason="test shell")
-    def test_update(self):
-        """Test for this."""
-        pass
+    def test_raises_ConfigFormatError_with_bad_yaml_input(self, test_config):
+        with pytest.raises(ConfigFormatError):
+            test_config(yaml_input="""
+            - un
+            - deux
+            - trois
+            """)
 
+    def test_passing_mapping_to_update_adds_keys_as_attrs_with_values(self, test_config):
+        """Test the update method adds key value pairs as new class attributes."""
+        conf = test_config()
+        conf.update({
+            'beaches': ['porthcurno', 'sennen'],
+            'roads': 'A30',
+        })
+        assert conf.beaches == ['porthcurno', 'sennen']
+        assert conf.roads == 'A30'
 
-class TestSelectedScenarioConfig:
-    """Group of tests for SelectedScenarioConfig."""
+    def test_set_attrs_just_updates_if_to_unpack_not_given(self, test_config):
+        """Test set_attrs method behaves like update method when to_unpack not given."""
+        conf1 = test_config()
+        # Loads the same config file as test_config does.
+        conf2 = Config('my_config')
 
-    @pytest.mark.skip(reason="test shell")
-    def test_init(self):
-        """Test for SelectedScenarioConfig."""
-        pass
+        attrs = {'colour': 'green', 'shape': 'circle'}
+        conf1.set_attrs(attrs)
+        conf2.update(attrs)
+
+        assert vars(conf1) == vars(conf2)
+
+    def test_set_attrs_unpacks_given_mapping_attrs_directly(self, test_config):
+        """Test set_attrs method unpacks mappings at attrs given by
+        to_unpack directly as attributes.
+        """
+        conf = test_config()
+        attrs = {
+            'names': {'jenny': 'female', 'bruce': 'male'},
+            'kids': {'terrence': {'age': 12}},
+        }
+        conf.set_attrs(attrs, to_unpack=['names'])
+
+        attr_keys = vars(conf).keys()
+        assert 'jenny' in attr_keys
+        assert 'bruce' in attr_keys
+        # The mapping at 'names' is unpacked directly to the attributes above.
+        assert 'names' not in attr_keys
+        assert 'kids' in attr_keys
+
+    def test_set_attrs_raises_TypeError_when_attr_to_unpack_is_not_mapping(
+        self, test_config
+    ):
+        """Tests raises TypeError because 'rice' attr is not a mapping"""
+        conf = test_config()
+        attrs = {'rice': ['uncle_bens', 'tilda'], 'pasta': ['napolina']}
+        with pytest.raises(TypeError):
+            conf.set_attrs(attrs, to_unpack=['rice'])
+
+    @pytest.mark.parametrize('bad_attr', ['yelp', [1, 2, 3], 5.67])
+    def test_set_attrs_raises_ConfigFormatError_when_not_passed_mapping(
+        self, test_config, bad_attr
+    ):
+        """Tests raises ConfigFormatError when using set_attrs."""
+        conf = test_config()
+        with pytest.raises(ConfigFormatError):
+            conf.set_attrs(bad_attr)
+
+    def test_flatten_nested_dicts_only_flattens_given_attrs(self, test_config):
+        """Test flatten_nested_dicts flattens the mappings at the given attrs."""
+        conf = test_config(yaml_input="""
+        bells:
+            big_ben:
+                dongs: 12
+        appearances:
+            batman:
+                joker: 27
+                deadshot: 7
+                killer_croc: 12
+        """)
+        conf.flatten_nested_dicts(['bells'])
+        assert conf.bells == {('big_ben', 'dongs') : 12}
+        # Check appearances unchanged.
+        assert conf.appearances == {'batman': {'joker': 27, 'deadshot': 7, 'killer_croc': 12}}
+
+    def test_get_key_value_pairs_returns_pairs_for_given_attrs(
+        self, test_config, all_in_output
+    ):
+        """Test returns key value pairs for specified attributes."""
+        conf = test_config(yaml_input="""
+        paris:
+            landmarks:
+                - arc de triomphe
+                - eiffel tower
+            museums:
+                - le louvre
+        london:
+            landmarks:
+                - big ben
+                - st paul's cathedral
+        """)
+        conf.get_key_value_pairs(['paris'])
+        assert all_in_output(
+            output=conf.paris,
+            values=[
+                ('landmarks', 'arc de triomphe'),
+                ('landmarks', 'eiffel tower'),
+                ('museums', 'le louvre'),
+            ],
+        )
+        # Check London unchanged.
+        assert conf.london == {'landmarks': ['big ben', 'st paul\'s cathedral']}
+
+    def test_fill_tuples(self, test_config):
+        """Test fill_tuples works in place for config attributes."""
+        conf = test_config()
+        conf.update({'scanner': ['retailer_1', 'retailer_2', 'retailer_3']})
+        conf.fill_tuples(['scanner'], repeat=True, length=2)
+        assert conf.scanner == [
+            ('retailer_1', 'retailer_1'),
+            ('retailer_2', 'retailer_2'),
+            ('retailer_3', 'retailer_3'),
+        ]
+
+    def test_fill_tuple_keys(self, test_config):
+        """Test fill_tuple_keys works in place for config attributes."""
+        conf = test_config()
+        conf.update({
+            'food': {
+                ('burger', 'patty', 'meat', 'beef'): 4,
+                ('burger', 'patty', 'veggie', 'bean'): 3,
+                ('burger', 'sauces', 'spicy', 'piri piri'): 1,
+                ('hotdog', 'sausage', 'pork',): 3,
+                ('chips'): 2
+            }
+        })
+        conf.fill_tuple_keys(['food'], repeat=True)
+        assert conf.food == {
+            ('burger', 'patty', 'meat', 'beef'): 4,
+            ('burger', 'patty', 'veggie', 'bean'): 3,
+            ('burger', 'sauces', 'spicy', 'piri piri'): 1,
+            ('hotdog', 'hotdog', 'sausage', 'pork',): 3,
+            ('chips', 'chips', 'chips', 'chips'): 2
+        }
 
 
 class TestScenarioConfig:
     """Group of tests for ScenarioConfig."""
 
     @pytest.mark.skip(reason="test shell")
-    def test_init(self):
-        """Test for ScenarioConfig."""
-        pass
-
-    @pytest.mark.skip(reason="test shell")
     def test_validate(self):
         """Test for this."""
-        pass
-
-
-class TestDevConfig:
-    """Group of tests for DevConfig."""
-
-    @pytest.mark.skip(reason="test shell")
-    def test_init(self):
-        """Test for DevConfig."""
         pass
 
 
