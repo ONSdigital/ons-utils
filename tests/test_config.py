57bd31294ffee0d37a717813fb052e2d173bedd0
edmunm <Mitchell.Edmunds@ext.ons.gov.uk>
2021-07-19 10:38:24 +0000

    PADSP-1416: Update config test docstrings

--- tests/test_config.py
+++ tests/test_config.py
@@ -6,27 +6,30 @@ from cprices.config import *
 from cprices import config
 
 
-def write_config_yaml(dir, yaml_input):
-    """ """
+def write_config_yaml(dir, yaml_input: str) -> None:
+    """Write a file called my_config.yaml with given yaml at given dir."""
     with open(dir.join('my_config.yaml'), 'w') as f:
         yaml.dump(yaml.safe_load(yaml_input), f)
 
 
 @pytest.fixture
 def test_config(tmpdir, monkeypatch):
-    """ """
-    def _(yaml_input="template_text"):
+    """Sets up a test config file in tmpdir/config with given yaml."""
+    def _(yaml_input: str = "template_text") -> Config:
         config_dir = tmpdir.mkdir('config')
         monkeypatch.setattr(config.Path, 'home', lambda: Path(tmpdir))
         write_config_yaml(config_dir, yaml_input=yaml_input)
         return Config('my_config')
     return _
 
+
 class TestConfig:
     """Group of tests for Config."""
 
     def test_init_assigns_name_and_config_path_attrs(self, test_config, tmpdir):
-        """Test for this."""
+        """Test that the name and config attributes are assigned after init."""
+        # test_config creates a config file 'my_config.yaml' in the dir
+        # tmpdir/config.
         conf = test_config()
         assert conf.name == 'my_config'
         assert conf.config_path == Path(tmpdir).joinpath('config', 'my_config.yaml')
@@ -34,7 +37,9 @@ class TestConfig:
     def test_get_config_dir_when_cprices_config_env_variable_set(
         self, tmpdir, monkeypatch
     ):
-        """Test for this."""
+        """Patch the CPRICES_CONFIG env variable and assert get_config_dir method
+        returns the locations given by the env var.
+        """
         monkeypatch.setenv('CPRICES_CONFIG', str(tmpdir))
         write_config_yaml(tmpdir, yaml_input="template_text")
         conf = Config('my_config')
@@ -43,15 +48,16 @@ class TestConfig:
     def test_get_config_dir_returns_cprices_cprices_first_if_exists(
         self, tmpdir, monkeypatch,
     ):
-        """Test for this."""
+        """Test that get_config_dir returns the config dir in the nested
+        folder cprices/cprices ahead of returning a config dir in the current
+        working directory or the home locations. This is how the current nesting
+        is on DAP.
+        """
         target_dir = tmpdir.mkdir('cprices').mkdir('cprices').mkdir('config')
         tmpdir.mkdir('config')
 
-        def return_tmpdir():
-            return Path(tmpdir)
-
-        monkeypatch.setattr(config.Path, 'cwd', return_tmpdir)
-        monkeypatch.setattr(config.Path, 'home', return_tmpdir)
+        monkeypatch.setattr(config.Path, 'cwd', lambda: Path(tmpdir))
+        monkeypatch.setattr(config.Path, 'home', lambda: Path(tmpdir))
 
         write_config_yaml(target_dir, yaml_input="template_text")
 
@@ -60,27 +66,26 @@ class TestConfig:
     def test_get_config_dir_returns_home_dir_config_if_no_cprices_dir(
         self, tmpdir, monkeypatch,
     ):
-        """Test for this."""
+        """Test that get_config_dir returns home before current working directory."""
         target_dir = tmpdir.mkdir('config')
 
-        def return_tmpdir():
-            return Path(tmpdir)
-
-        monkeypatch.setattr(config.Path, 'cwd', return_tmpdir)
-        monkeypatch.setattr(config.Path, 'home', return_tmpdir)
+        monkeypatch.setattr(config.Path, 'cwd', lambda: Path(tmpdir))
+        monkeypatch.setattr(config.Path, 'home', lambda: Path(tmpdir))
 
         write_config_yaml(target_dir, yaml_input="template_text")
 
         assert Config('my_config').get_config_dir() == target_dir
 
     def test_get_config_path(self, test_config, tmpdir):
-        """Test for this."""
+        """Test get_config_path returns the path of the given config file."""
+        # test_config creates a config file 'my_config.yaml' in the dir
+        # tmpdir/config.
         conf = test_config()
         expected = Path(tmpdir).joinpath('config', 'my_config.yaml')
         assert conf.get_config_path() == expected
 
     def test_load_config(self, test_config):
-        """Test for this."""
+        """Test load_config method loads YAML input and returns a dict."""
         conf = test_config(yaml_input="""
         bells:
             big_ben:
