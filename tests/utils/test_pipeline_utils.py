8d233953f618358f630a30381f390c6c47ed9a6f
edmunm <Mitchell.Edmunds@ext.ons.gov.uk>
2021-08-23 07:26:46 +0000

    PADSP-1579: Add check_empty function to pipeline_utils

--- tests/utils/test_pipeline_utils.py
+++ tests/utils/test_pipeline_utils.py
@@ -1,5 +1,8 @@
 """Tests for pipeline utils."""
+from pyspark.sql.types import *
 import pytest
+
+from cprices.utils.pipeline_utils import *
 
 
 @pytest.mark.skip(reason="test shell")
@@ -30,3 +33,16 @@ def test_get_run_times_as_df():
 def test_plot_run_times():
     """Test for this."""
     pass
+
+
+def test_check_empty(spark_session):
+    """Test the check_empty function raises DataFrameEmptyError."""
+    # Create an empty DataFrame.
+    empty_RDD = spark_session.sparkContext.emptyRDD()
+    df = spark_session.createDataFrame(
+        data=empty_RDD,
+        schema=StructType([]),
+    )
+
+    with pytest.raises(DataFrameEmptyError):
+        check_empty(df)
