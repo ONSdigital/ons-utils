9dea3d09b7c028ee32b8a7ab1169d7ce37d5f211
edmunm <Mitchell.Edmunds@ext.ons.gov.uk>
2021-08-27 15:47:27 +0000

    PADSP-1354: Add missing root '/' to expected in spark tests

--- tests/utils/test_spark_setup.py
+++ tests/utils/test_spark_setup.py
@@ -12,7 +12,7 @@ from tests.conftest import parametrize_cases, Case
     Case('local_python', env_var='/usr/local/bin/python3.6'),
     Case(
         'lower_miscmods_ver',
-        env_var='opt/ons/virtualenv/miscMods_v2.05/bin/python3.6',
+        env_var='/opt/ons/virtualenv/miscMods_v2.05/bin/python3.6',
     ),
 )
 def test_set_pyspark_python_env_sets_right_path(monkeypatch, env_var):
@@ -20,18 +20,18 @@ def test_set_pyspark_python_env_sets_right_path(monkeypatch, env_var):
     monkeypatch.setenv("PYSPARK_PYTHON", env_var)
     set_pyspark_python_env(3.05)
 
-    assert os.getenv('PYSPARK_PYTHON') == 'opt/ons/virtualenv/miscMods_v3.05/bin/python3.6'
+    assert os.getenv('PYSPARK_PYTHON') == '/opt/ons/virtualenv/miscMods_v3.05/bin/python3.6'
 
 
 def test_doesnt_set_pyspark_python_env_when_already_later_version(monkeypatch):
     """Test that the PYSPARK_PYTHON is set given the input."""
     monkeypatch.setenv(
         "PYSPARK_PYTHON",
-        'opt/ons/virtualenv/miscMods_v3.10/bin/python3.6',
+        '/opt/ons/virtualenv/miscMods_v3.10/bin/python3.6',
     )
     set_pyspark_python_env(3.05)
 
-    assert os.getenv('PYSPARK_PYTHON') == 'opt/ons/virtualenv/miscMods_v3.10/bin/python3.6'
+    assert os.getenv('PYSPARK_PYTHON') == '/opt/ons/virtualenv/miscMods_v3.10/bin/python3.6'
 
 
 @pytest.mark.spark
