e1d61e55650e302b1a660f8fc3c3746825b10562
edmunm <Mitchell.Edmunds@ext.ons.gov.uk>
2021-09-24 11:07:05 +0000

    PADSP-1580: Fix small error in timer_args

--- cprices/alternative_sources/index_methods/_utils.py
+++ cprices/alternative_sources/index_methods/_utils.py
@@ -36,7 +36,7 @@ def timer_args(name, logger):
         'name': name,
         'text': lambda secs: name + (
             # Returns minutes and seconds if at least 1 minute passed.
-            f": {secs/60:.0f} mins {secs:.2f} secs" if secs/60 > 1
+            f": {secs/60:.0f} mins {secs%60:.2f} secs" if secs/60 > 1
             else f": {secs:.2f} secs"
         ),
         'logger': logger,
